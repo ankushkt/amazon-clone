2dcbb804434a2803680f0213438a15f078599583
ankushkt <at2619760@gmail.com>
2024-01-09 20:07:08 +0530

    create navbar part -2

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,26 +1,32 @@
 import React from 'react'
+import './Header.css'
 
 function Header() {
   return (
     <>
       <header>
         <div className=' flex bg-black h-auto'>
-          <div className='flex  align-center hover:border-solid hover:border-2 hover:border-white'>
+          <div className='flex  align-center'>
             <img className='size-26 ml-3 mt-1' src="https://raw.githubusercontent.com/myaduvanshi/amazon-prime/main/src/Components/Images/amazonLogo.svg" alt="" />
             <span className='text-white text-sm font-normal mt-5   '>.in</span>
           </div>
-          <div className='ml-7'>
-            <div className='flex mt-3 text-small mb-3 hover:border-solid hover:border-2 hover:border-white'>
+          <div className='ml-4'>
+            <div className='flex mt-3 text-small mb-3'>
               <img className='size-5 mt-4 ' src="https://raw.githubusercontent.com/myaduvanshi/amazon-prime/main/src/Components/Images/location.svg" alt="" />
               <p className='text-white text-sm  font-thin'>Delivering to Delhi 110002 <br />
-              <span className='text-white font-bold text-base'>Updated location </span></p>
+                <span className='text-white font-bold text-base'>Updated location </span></p>
             </div>
-            <div>
+          </div>
+          <div className='flex border-white w-80 h-12 relative top-2 ml-3 rounded-md'>
+            <div className=' w-16 h-full border-white '>
+              <select className='w-full h-full text-balck border-none rounded-1-sm bg-gay-100 px-2' name="category" id="category">
+                <option value="All categories">All categories</option>
+
+              </select>
+              
 
             </div>
-          
-
-
+            
 
           </div>
         </div>
