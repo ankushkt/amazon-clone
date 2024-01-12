8ae067086bc4dcb95b01df08969b66a50bb8a682
ankushkt <at2619760@gmail.com>
2024-01-12 20:02:54 +0530

    navbar part 2

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -17,18 +17,29 @@ function Header() {
                 <span className='text-white font-bold text-base'>Updated location </span></p>
             </div>
           </div>
-          <div className='flex border-white w-80 h-12 relative top-2 ml-3 rounded-md'>
-            <div className=' w-16 h-full border-white '>
-              <select className='w-full h-full text-balck border-none rounded-1-sm bg-gay-100 px-2' name="category" id="category">
-                <option value="All categories">All categories</option>
-
+          <div className='flex w-80 h-12 relative top-2 ml-4 rounded-md '>
+            <div className=' w-14 h-full '>
+              <select className='w-full h-full text-balck border-none rounded-sm bg-gray-200 px-2' name="category" id="category">
+                <option value="allCategories">All categories</option>
+                <option value="alexaSkills">Alexa Skills</option>
+                <option value="amazonDevice">Amazon Device</option>
+                <option value="amazonFashion">Amazon Fashion</option>
+                <option value="amazonFresh">Amazon Fresh</option>
               </select>
-              
-
             </div>
-            
+            <div className='bg-white w-48  flex  items-center '>
+              <span className='ml-3 font-bold text-lg'> Search Amazon.in   </span>
+            </div>
+            <div className='w-14 bg-yellow-600 '>
+              <img className='relative top-3 left-4' src="https://raw.githubusercontent.com/myaduvanshi/amazon-prime/main/src/Components/Images/seacrh.svg" alt="" />
+            </div>
+          </div>
+          <div className='text-white flex  border-white w-14 p-3'>
+            <img src="https://raw.githubusercontent.com/myaduvanshi/amazon-prime/main/src/Components/Images/indiaFlagIcon.svg" alt="indian flag"  />
+            <span>ENG</span>
 
           </div>
+
         </div>
       </header>
     </>
