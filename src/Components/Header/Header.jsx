5ad4a8b4757993b7b88864b6aa1bc17e4a334513
ankushkt <at2619760@gmail.com>
2024-01-23 20:05:48 +0530

    navbar part-5

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import './Header.css'
+import { Link } from 'react-router-dom'
 
 function Header() {
   return (
@@ -34,21 +35,39 @@ function Header() {
               <img className='relative top-3 left-4' src="https://raw.githubusercontent.com/myaduvanshi/amazon-prime/main/src/Components/Images/seacrh.svg" alt="" />
             </div>
           </div>
-          <div className='dropdowm'>
+          <div className='dropdown'>
             <div className=' flex w-14 border-none justify-center items-center ml-0 mt-5'>
               <img className='size-5' src="https://raw.githubusercontent.com/myaduvanshi/amazon-prime/main/src/Components/Images/indiaFlagIcon.svg" alt="indian flag" />
               <span className='text-white '>EN</span>
             </div>
             <div className="dropdown-content text-black list-none">
-          <li> <Link to=""> <input type="radio" name="localLang" id="localLang" checked />English- EN </Link></li>
-                            <li> <Link to=""> <input type="radio" name="localLang" id="localLang" />हिन्दी - Hi </Link></li>
-                            <li> <Link to=""> <input type="radio" name="localLang" id="localLang" />தமிழ் - TA </Link></li>
-                            <li> <Link to=""> <input type="radio" name="localLang" id="localLang" />తెలుగు - TE </Link></li>
-                            <li> <Link to=""> <input type="radio" name="localLang" id="localLang" />ಕನ್ನಡ - KN </Link></li>
-                            <li> <Link to=""> <input type="radio" name="localLang" id="localLang" />മലയാളം - ML </Link></li> 
-                            <li> <Link to=""> <input type="radio" name="localLang" id="localLang" />বাংলা - BN </Link></li>
-                            <li> <Link to=""> <input type="radio" name="localLang" id="localLang" />मराठी - MR </Link></li>
+              <li> <Link to=""> <input type="radio" name="localLang" id="localLang" checked />English- EN </Link></li>
+              <li> <Link to=""> <input type="radio" name="localLang" id="localLang" />हिन्दी - Hi </Link></li>
+              <li> <Link to=""> <input type="radio" name="localLang" id="localLang" />தமிழ் - TA </Link></li>
+              <li> <Link to=""> <input type="radio" name="localLang" id="localLang" />తెలుగు - TE </Link></li>
+              <li> <Link to=""> <input type="radio" name="localLang" id="localLang" />ಕನ್ನಡ - KN </Link></li>
+              <li> <Link to=""> <input type="radio" name="localLang" id="localLang" />മലയാളം - ML </Link></li>
+              <li> <Link to=""> <input type="radio" name="localLang" id="localLang" />বাংলা - BN </Link></li>
+              <li> <Link to=""> <input type="radio" name="localLang" id="localLang" />मराठी - MR </Link></li>
+            </div>
           </div>
+          <div className='dropdown'>
+            <div className=' w-full ml-5 leading-none pt-4 '>
+              <p className='text-xs text-white'> Hello,Sign in</p>
+              <h3 className='text-white font-bold'>Accounts & List</h3>
+            </div>
+            <div className='dropdown-content text-black list-none mt-1'>
+              <div className='flex w-full justify-center'>
+                <Link to="">
+                  <button className='bg-[#FFD814] w-32 p-2 rounded-md font-bold'>
+                    Sign in
+                  </button>
+                </Link>
+
+              </div>
+
+            </div>
+
           </div>
         </div>
       </header >
